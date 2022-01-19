9dc9e0cbda42438b3bc1a5bb51e1fac13ee370c9
Adriana <aguilaradry7@gmail.com>
2022-01-18 23:01:10 -0500

    :tada:first commit

---
+++ src/main/java/co/edu/iudigital/app/BackendDelitosApplication.java
@@ -0,0 +1,14 @@
+package co.edu.iudigital.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDelitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDelitosApplication.class, args);
+		System.out.println("API Cargada...");
+	}
+
+}
