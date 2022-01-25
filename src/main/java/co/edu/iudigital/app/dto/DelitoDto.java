e9a41d2bbeefa1f1ad3dc27e4a0e6cc9417d3189
Adriana <aguilaradry7@gmail.com>
2022-01-24 22:36:54 -0500

    :sparkles:implementing DelitoserviceImpl and DelitoController

--- src/main/java/co/edu/iudigital/app/dto/DelitoDto.java
+++ src/main/java/co/edu/iudigital/app/dto/DelitoDto.java
@@ -3,11 +3,49 @@ package co.edu.iudigital.app.dto;
 import lombok.Builder;
 import lombok.Data;
 
-@Data //crea getter y setter por lombok
-@Builder //Construye un objeto
+//@Data //crea getter y setter por lombok
+//@Builder //Construye un objeto
 public class DelitoDto {
 
 	private Long id;
 	private String nombre;
 	private String description;
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
 }
